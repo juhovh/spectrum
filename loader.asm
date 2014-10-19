8d0efeae02390462290ea0a9eb59c6abeea16392
Juho Vähä-Herttua <juhovh@iki.fi>
2014-10-19 11:56:04 +0300

    Code style cleanup in the loader.

--- loader.asm
+++ loader.asm
@@ -1,102 +1,88 @@
-org $8000
 
-REPT 257
-db $81 ; interrupt handler at $8181
-ENDM
+; pointers to system ROM routines
+ROM_CLS	equ $0d6b
+ROM_CHAN_OPEN	equ $1601
 
-ROM_CLS       EQU $0D6B
-ROM_CHAN_OPEN EQU $1601
+; interrupt jump table to $8181
+	org $8000
+	rept 257
+	db $81
+	endm
 
-loader:
- ; clear screen
- call ROM_CLS
+; main loader routine
+loader	call ROM_CLS
+	ld a,2
+	call ROM_CHAN_OPEN
 
- ; open channel 2
- ld a,2
- call ROM_CHAN_OPEN
+	ld b,11
+ldr_nl_loop	ld a,13
+	rst 16
+	djnz ldr_nl_loop
 
- ; add 11 newlines
- ld b,11
-ldr_nl_loop:
- ld a,13
- rst 16
- djnz ldr_nl_loop
+	ld hl,ldr_str
+ldr_str_loop	ld a,(hl)
+	cp 0
+	jr z,ldr_kb_loop
+	rst 16
+	inc hl
+	jr ldr_str_loop
 
- ; print string
- ld hl,ldr_string
-ldr_str_loop:
- ld a,(hl)
- cp 0
- jr z,ldr_kb_loop
- rst 16
- inc hl
- jr ldr_str_loop
+ldr_kb_loop	halt
+	xor a
+	in a,($fe)
+	cpl
+	and 15
+	jr z,ldr_kb_loop
 
- ; wait for keypress
-ldr_kb_loop:
- halt
- xor a
- in a,($fe)
- cpl
- and 15
- jr z,ldr_kb_loop
+	di
+	push af
+	push bc
+	push de
+	push hl
+	push ix
 
- di
- ; store main registers
- push af
- push bc
- push de
- push hl
- push ix
+	exx
+	ex af,af'
 
- exx
- ex af,af'
+	push af
+	push bc
+	push de
+	push hl
+	push iy
 
- ; store shadow registers
- push af
- push bc
- push de
- push hl
- push iy
+	call init
 
- call init
+	ld a,$80
+	ld i,a
+	im 2
+	ei
 
- ; enable interrupt table at $8000
- ld a,$80
- ld i,a
- im 2
- ei
+	call main
 
- call main
+	di
+	im 1
 
- di
- ; return to im 1 mode
- im 1
+	call clear
 
- call clear
+	pop iy
+	pop hl
+	pop de
+	pop bc
+	pop af
 
- ; restore shadow registers
- pop iy
- pop hl
- pop de
- pop bc
- pop af
+	ex af,af'
+	exx
 
- exx
- ex af,af'
+	pop ix
+	pop hl
+	pop de
+	pop bc
+	pop af
+	ei
+	ret
 
- ; restore main registers
- pop ix
- pop hl
- pop de
- pop bc
- pop af
- ei
- ret
-
-ldr_string:
-db '     PRESS ANY KEY TO START     ',0
+ldr_str	db '     PRESS ANY KEY TO START     ',0
 
 ; our custom interrupt handler
-org $8181
- jp interrupt
+	org $8181
+	jp interrupt
