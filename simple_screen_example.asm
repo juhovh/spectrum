3a15df0e60c87a130d061d1f43249b683b94f4e5
Juho Vähä-Herttua <juhovh@iki.fi>
2014-11-22 13:43:18 +0200

    Add a simple example of modifying screen colours.

---
+++ simple_screen_example.asm
@@ -0,0 +1,39 @@
+org $8000	
+include utils.asm
+
+ourcolour	defl PAPER_BLACK+INK_BLUE+COLOUR_BRIGHT
+
+main:
+	;; Clear everything on the screen
+	call ClearScreen
+
+	;; Set border colour to black
+	ld a,INK_BLACK
+	out ($fe),a
+
+	;; We want to write 768 bytes to $5800
+	ld hl,$5800
+	ld bc,768
+loop:
+	;; Load the colour we want
+	ld (hl),ourcolour
+	;; Increase hl and decrease bc
+	inc hl
+	dec bc
+	;; Check if bc is completely 0
+	ld a,b
+	or c
+	;; Continue loop if not 0
+	jr nz,loop
+
+	;; Set some pixels to check that our blue colour works
+	ld b,0
+	ld c,0
+	call SetPixel
+	inc b
+	inc c
+	call SetPixel
+	di
+	halt
+
+end main
